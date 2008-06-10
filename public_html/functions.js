0ebc88307fdae6d4ee713f2437acdf92412bee76
Dustin Kirkland <dustin@server>
2008-06-10 17:44:47 -0500

    moved javascript out to includable file, updated above.html accordingly

---
+++ public_html/functions.js
@@ -0,0 +1,20 @@
+function highlight(word) {
+        if (location.href.match("/" + word)) {
+                return("current");
+        } else {
+                return("plain");
+        }
+}
+function navbar() {
+        document.write("<ul>");
+        versions = new Array();
+        versions.push({"name":"dapper", "number":"6.06 LTS"});
+        versions.push({"name":"feisty", "number":"7.04"});
+        versions.push({"name":"gutsy", "number":"7.10"});
+        versions.push({"name":"hardy", "number":"8.04 LTS"});
+        versions.push({"name":"intrepid", "number":"8.10"});
+        for (var i=0; i<versions.length; i++) {
+                document.write("<li id=" + highlight(versions[i]["name"]) + "><a href=/manpages/" + versions[i]["name"] + ">" + versions[i]["number"] + "</a></li>");
+        }
+        document.write("</ul>");
+}
