495393910519befd975a9bad06e86ceebd2e65f9
Dustin Kirkland <dustin@server>
2008-06-10 12:44:03 -0500

    have sitemap generation source a new config file

--- src/make-sitemaps.sh
+++ src/make-sitemaps.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-SITE="http://ubuntu.dustinkirkland.com"
+. ./config 
 
-
+cd $PUBLIC_HTML_DIR
 find manpages/ -type f -name "*.html" | xargs -i echo "<url><loc>$SITE/{}</loc></url>" | split -l 50000 - manpages/sitemap_
 
-sitemaps=`ls sitemap_??`
+sitemaps=`ls manpages/sitemap_??`
 for i in $sitemaps; do
 	echo '<?xml version="1.0" encoding="UTF-8"?>' > $i.xml
 	echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> $i.xml
@@ -13,3 +13,4 @@ for i in $sitemaps; do
 	echo '</urlset>' >> $i.xml
 	rm -f $i
 done
+cd -
