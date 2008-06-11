94317a07c304c40ac4fd807e6995b29876f130a0
Dustin Kirkland <dustin@server>
2008-06-10 23:30:55 -0500

    create the .cache directory as well

--- src/make-manpage-repo.sh
+++ src/make-manpage-repo.sh
@@ -3,7 +3,7 @@
 . ./config
 
 for dist in $DISTROS; do
-	mkdir -p $PUBLIC_HTML_DIR/manpages/$dist
+	mkdir -p $PUBLIC_HTML_DIR/manpages/$dist/.cache
 	for repo in $REPOS; do
 		DEBS=`cat $DEBDIR/dists/$dist/$repo/binary-$ARCH/Packages.gz | gunzip | grep "^Filename:.*\.deb$" | awk '{print $2}' | sort -u`
 		for deb in $DEBS; do
