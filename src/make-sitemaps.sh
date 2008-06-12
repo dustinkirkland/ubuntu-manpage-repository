0f65c57acf6c25344c3f8dc9f5b37e452da29bbc
Dustin Kirkland <dustin@server>
2008-06-11 23:34:39 -0500

    move echo, add comment

--- src/make-sitemaps.sh
+++ src/make-sitemaps.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# Generate sitemaps per sitemaps.org for google.com
+
 . ./config 
+
+echo "INFO: Making sitemaps"
 
 cd $PUBLIC_HTML_DIR
 find manpages/ -type f -name "*.html" | xargs -i echo "<url><loc>$SITE/{}</loc></url>" | split -l 50000 - manpages/sitemap_
 
-echo "INFO: Making sitemaps"
 sitemaps=`ls manpages/sitemap_??`
 for i in $sitemaps; do
 	echo '<?xml version="1.0" encoding="UTF-8"?>' > $i.xml
