8f5ee19fa2536728f9aca6320f0175ad4c003af9
Dustin Kirkland <dustin@server>
2008-06-17 15:32:41 -0500

    fix a couple of minor bugs affecting the manpage whitespace cleanup

--- src/w3mman-to-html.pl
+++ src/w3mman-to-html.pl
@@ -3,22 +3,18 @@
 @stdin = <STDIN>;
 shift(@stdin);
 shift(@stdin);
-$title = " - $stdin[1]";
 
 for ($i=0; $i<@stdin; $i++) {
 	$stdin[$i] =~ s/</&lt;/g;
 	$stdin[$i] =~ s/^([A-Z].+)\n+/<\/pre><h3>$1<\/h3><pre>/g;
 	$stdin[$i] =~ s/\s([^\s]+)\(([1-9])\)/ <a href=..\/man$2\/$1.html>$1($2)<\/a>/g;
 	$stdin[$i] =~ s/\s[<]?([a-zA-Z]+):\/\/([^\s>]+)[>]?/ <a href=$1:\/\/$2>$1:\/\/$2<\/a>/g;
-	if ($stdin[$i] =~ /^\s*<\/pre>/) {
+	if ($i>0 && $stdin[$i] =~ /^\s*<\/pre>/) {
 		# Remove blank lines preceding a line starting with </pre>
 		if ($stdin[$i-1] =~ /^\s*$/) {
 			splice(@stdin, $i-1, 1);
+			$i--;
 		}
-	}
-	if ($stdin[$i] =~ /^\s*<\/pre>/ && $stdin[$i-1] =~ /<pre>\s*$/) {
-		$stdin[$i-1] =~ s/<pre>\s*$//;
-		$stdin[$i] =~ s/^\s*<\/pre>//;
 	}
 }
 
