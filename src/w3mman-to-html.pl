62f1adddad98f2c21bcbea56fb7774e2aad3a1f3
Dustin Kirkland <dustin@server>
2008-06-16 22:21:13 -0500

    handle < in manpages print package name and link at top

--- src/w3mman-to-html.pl
+++ src/w3mman-to-html.pl
@@ -6,6 +6,7 @@ shift(@stdin);
 $title = " - $stdin[1]";
 
 for ($i=0; $i<@stdin; $i++) {
+	$stdin[$i] =~ s/</&lt;/g;
 	$stdin[$i] =~ s/^([A-Z].+)\n+/<\/pre><h3>$1<\/h3><pre>/g;
 	$stdin[$i] =~ s/\s([^\s]+)\(([1-9])\)/ <a href=..\/man$2\/$1.html>$1($2)<\/a>/g;
 	$stdin[$i] =~ s/\s[<]?([a-zA-Z]+):\/\/([^\s>]+)[>]?/ <a href=$1:\/\/$2>$1:\/\/$2<\/a>/g;
@@ -13,7 +14,6 @@ for ($i=0; $i<@stdin; $i++) {
 		# Remove blank lines preceding a line starting with </pre>
 		if ($stdin[$i-1] =~ /^\s*$/) {
 			splice(@stdin, $i-1, 1);
-			$i--;
 		}
 	}
 	if ($stdin[$i] =~ /^\s*<\/pre>/ && $stdin[$i-1] =~ /<pre>\s*$/) {
@@ -22,7 +22,11 @@ for ($i=0; $i<@stdin; $i++) {
 	}
 }
 
-unshift(@stdin, '<!--#include virtual="/above.html" --><pre>');
+$pkg = $ARGV[0];
+$pkg_name = $ARGV[0];
+$pkg_name =~ s/_.*$//g;
+
+unshift(@stdin, '<!--#include virtual="/above.html" -->Provided by: <a href=http://launchpad.net/ubuntu/+source/' . $pkg_name . '>' . $pkg . '</a><pre>');
 push(@stdin, '</pre><!--#include virtual="/below.html" -->');
 
 print("@stdin");
