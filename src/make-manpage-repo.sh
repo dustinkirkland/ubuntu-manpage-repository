78da4650b918e08e0accd39f97329f0c3a19f2ca
Dustin Kirkland <dustin@server.dustinkirkland.com>
2008-07-22 19:40:24 -0500

    per kees' audit:  * quote all variable usages  * use zcat | grep | awk | sort | while to avoid too many args in a for loop

--- src/make-manpage-repo.sh
+++ src/make-manpage-repo.sh
@@ -8,9 +8,10 @@ pkg_updated() {
 	if [ "$FORCE" = "-f" ]; then
 		return 0
 	fi
+	deb="$1"
 	#echo "INFO: Looking at package [$deb]"
 	name=`basename "$deb" | awk -F_ '{print $1}'`
-	cache_modtime=`(ls -log --time-style=+%s $PUBLIC_HTML_DIR/manpages/$dist/.cache/$name 2>/dev/null || echo "0 0 0 0") | awk '{print $4}'`
+	cache_modtime=`(ls -log --time-style=+%s "$PUBLIC_HTML_DIR/manpages/$dist/.cache/$name" 2>/dev/null || echo "0 0 0 0") | awk '{print $4}'`
 	deb_modtime=`ls -log --time-style=+%s "$DEBDIR/$deb" | awk '{print $4}'`
 	if [ "$cache_modtime" -ge "$deb_modtime" ]; then
 	        #echo "INFO: Skipping non-updated package [$DEBDIR/$deb]"
@@ -20,14 +21,21 @@ pkg_updated() {
 	fi
 }
 
+handle_deb() {
+	dist="$1"
+	deb="$2"
+	pkg_updated "$deb" && ./fetch-man-pages.sh "$dist" "$deb"
+}
+
 
 for dist in $DISTROS; do
-	mkdir -p $PUBLIC_HTML_DIR/manpages/$dist/.cache $PUBLIC_HTML_DIR/manpages.gz/$dist
+	export dist
+	mkdir -p "$PUBLIC_HTML_DIR/manpages/$dist/.cache $PUBLIC_HTML_DIR/manpages.gz/$dist"
 	for repo in $REPOS; do
-		DEBS=`cat $DEBDIR/dists/$dist/$repo/binary-$ARCH/Packages.gz | gunzip | grep "^Filename:.*\.deb$" | awk '{print $2}' | sort -u`
-		for deb in $DEBS; do
-			pkg_updated && ./fetch-man-pages.sh $dist $deb
-		done
+		zcat "$DEBDIR/dists/$dist/$repo/binary-$ARCH/Packages.gz" | grep "^Filename:.*\.deb$" | awk '{print $2}' | sort -u | \
+			while read deb; do
+				handle_deb "$dist" "$deb"
+			done
 	done
 done
 
