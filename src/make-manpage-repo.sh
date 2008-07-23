efc8cba804e890952ca186a0d1a886a029229f5a
Dustin Kirkland <dustin@server.dustinkirkland.com>
2008-07-22 21:21:29 -0500

    use stat, instead of ls|awk

--- src/make-manpage-repo.sh
+++ src/make-manpage-repo.sh
@@ -37,8 +37,8 @@ pkg_updated() {
 	deb="$1"
 	#echo "INFO: Looking at package [$deb]"
 	name=`basename "$deb" | awk -F_ '{print $1}'`
-	cache_modtime=`(ls -log --time-style=+%s "$PUBLIC_HTML_DIR/manpages/$dist/.cache/$name" 2>/dev/null || echo "0 0 0 0") | awk '{print $4}'`
-	deb_modtime=`ls -log --time-style=+%s "$DEBDIR/$deb" | awk '{print $4}'`
+	cache_modtime=`stat -c %Y "$PUBLIC_HTML_DIR/manpages/$dist/.cache/$name" 2>/dev/null || echo "0"`
+	deb_modtime=`stat -c %Y "$DEBDIR/$deb"`
 	if [ "$cache_modtime" -ge "$deb_modtime" ]; then
 	        #echo "INFO: Skipping non-updated package [$DEBDIR/$deb]"
 		return 1
