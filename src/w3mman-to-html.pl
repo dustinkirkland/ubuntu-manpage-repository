8fcf70804a8ad9cbd8d6fee583a34e21cfd621ed
Dustin Kirkland <dustin@server.dustinkirkland.com>
2008-07-22 20:07:20 -0500

    per kees' audit, perl changes, using strict and escaping html

--- src/w3mman-to-html.pl
+++ src/w3mman-to-html.pl
@@ -1,14 +1,22 @@
 #!/usr/bin/perl
 
-@stdin = <STDIN>;
-shift(@stdin);
-shift(@stdin);
+use warnings;
+use strict;
+use CGI qw/escapeHTML/;
 
-for ($i=0; $i<@stdin; $i++) {
+my @stdin = <STDIN>;
+shift(@stdin);
+shift(@stdin);
+my $title = "Ubuntu Manpage - " . escapeHTML($stdin[1]);
+
+for (my $i=0; $i<@stdin; $i++) {
 	$stdin[$i] =~ s/</&lt;/g;
+# The following line would ideally be used instead, however it seems
+# to be too greedy with its escaping...
+#	$stdin[$i] =~ escapeHTML($stdin[$i]);
 	$stdin[$i] =~ s/^([A-Z].+)\n+/<\/pre><h3>$1<\/h3><pre>/g;
-	$stdin[$i] =~ s/\s([^\s]+)\(([1-9])\)/ <a href=..\/man$2\/$1.html>$1($2)<\/a>/g;
-	$stdin[$i] =~ s/\s[<]?([a-zA-Z]+):\/\/([^\s>]+)[>]?/ <a href=$1:\/\/$2>$1:\/\/$2<\/a>/g;
+	$stdin[$i] =~ s/\s([^\s]+)\(([1-9])\)/ <a href=\"..\/man$2\/$1.html\">$1($2)<\/a>/g;
+	$stdin[$i] =~ s/\s[<]?([a-zA-Z]+):\/\/([^\s>]+)[>]?/ <a href=\"$1:\/\/$2\">$1:\/\/$2<\/a>/g;
 	if ($i>0 && $stdin[$i] =~ /^\s*<\/pre>/) {
 		# Remove blank lines preceding a line starting with </pre>
 		if ($stdin[$i-1] =~ /^\s*$/) {
@@ -18,8 +26,8 @@ for ($i=0; $i<@stdin; $i++) {
 	}
 }
 
-$pkg = $ARGV[0];
-$pkg_name = $ARGV[0];
+my $pkg = $ARGV[0];
+my $pkg_name = $ARGV[0];
 $pkg_name =~ s/_.*$//g;
 
 unshift(@stdin, '<!--#include virtual="/above.html" -->Provided by: <a href=http://launchpad.net/ubuntu/+source/' . $pkg_name . '>' . $pkg . '</a><pre>');
