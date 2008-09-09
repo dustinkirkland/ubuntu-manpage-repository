e85e108840854d389522bea4f4dd80f4929bdfbf
Dustin Kirkland <kirkland@canonical.com>
2008-09-09 01:05:46 -0400

    fixes for (LP: #267076), restoring escaped characters in the html

--- bin/w3mman-to-html.pl
+++ bin/w3mman-to-html.pl
@@ -42,6 +42,9 @@ for (my $i=0; $i<@stdin; $i++) {
 	$stdin[$i] =~ s/^([A-Z].+)\n+/<\/pre><h3>$1<\/h3><pre>/g;
 	$stdin[$i] =~ s/\s([^\s]+)\(([1-9])\)/ <a href=\"..\/man$2\/$1.html\">$1($2)<\/a>/g;
 	$stdin[$i] =~ s/\s[<]?([a-zA-Z]+):\/\/([^\s>]+)[>]?/ <a href=\"$1:\/\/$2\">$1:\/\/$2<\/a>/g;
+	$stdin[$i] =~ s/\xe2\x94\xe2\x94\x82/|/g;		# pipe
+	$stdin[$i] =~ s/\xe2\x80\xe2\x80\x98/&#8216;/g; 	# left quote
+        $stdin[$i] =~ s/\xe2\x80\xe2\x80\x99/&#8217;/g;		# right quote
 	if ($i>0 && $stdin[$i] =~ /^\s*<\/pre>/) {
 		# Remove blank lines preceding a line starting with </pre>
 		if ($stdin[$i-1] =~ /^\s*$/) {
